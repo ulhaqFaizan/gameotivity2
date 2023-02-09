68b64be46ce0176ee78799c2e0e35c2ee375204b
Faizan ul haq <79333547+ulhaqFaizan@users.noreply.github.com>
2023-02-09 23:00:33 +0500

    Add files via upload

---
+++ precache-manifest.8bfb70ebc44eac03ee972681c3d6f021.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "43f767e8b9d1cf214eb1",
+    "url": "/gameotivity2/static/css/main.ae6218f9.chunk.css"
+  },
+  {
+    "revision": "43f767e8b9d1cf214eb1",
+    "url": "/gameotivity2/static/js/main.43f767e8.chunk.js"
+  },
+  {
+    "revision": "b2ac925eb2ab07c8c76a",
+    "url": "/gameotivity2/static/css/1.24ad5860.chunk.css"
+  },
+  {
+    "revision": "b2ac925eb2ab07c8c76a",
+    "url": "/gameotivity2/static/js/1.b2ac925e.chunk.js"
+  },
+  {
+    "revision": "37014579c31ea49d9fc9",
+    "url": "/gameotivity2/static/js/runtime~main.37014579.js"
+  },
+  {
+    "revision": "0e9be44e23b6f2394c3f56319e38613d",
+    "url": "/gameotivity2/static/media/home page bg.0e9be44e.png"
+  },
+  {
+    "revision": "e2ba85cd7c373f2e7420d6f557b382c2",
+    "url": "/gameotivity2/static/media/nft card.e2ba85cd.png"
+  },
+  {
+    "revision": "f8afa4f20b24243ad2ec8094b356898c",
+    "url": "/gameotivity2/static/media/Artboard 10.f8afa4f2.png"
+  },
+  {
+    "revision": "199d04f95e9aa379592a6a0efba43a6c",
+    "url": "/gameotivity2/static/media/chainlink.199d04f9.png"
+  },
+  {
+    "revision": "5aa116700a6d237efa7c97c8a4769031",
+    "url": "/gameotivity2/static/media/Lbank.5aa11670.png"
+  },
+  {
+    "revision": "ed626fecb4a2dac48647622d9dffc415",
+    "url": "/gameotivity2/static/media/polygon.ed626fec.svg"
+  },
+  {
+    "revision": "385809ac164c5ee313977cdc64d3d713",
+    "url": "/gameotivity2/static/media/nstp1.385809ac.png"
+  },
+  {
+    "revision": "71fb155e9abd03497e3aee0fc3f3c947",
+    "url": "/gameotivity2/static/media/airuni.71fb155e.png"
+  },
+  {
+    "revision": "1f3dd98a3608d12fdb05add007c0afe6",
+    "url": "/gameotivity2/static/media/kucoin.1f3dd98a.svg"
+  },
+  {
+    "revision": "a50fefb897181ddc311719d6a251e139",
+    "url": "/gameotivity2/static/media/networkprotocol.a50fefb8.png"
+  },
+  {
+    "revision": "8230ef97a135baffbb852ac4881f65ba",
+    "url": "/gameotivity2/static/media/3dmockup.8230ef97.png"
+  },
+  {
+    "revision": "c000a4f36aa1a77f49226b83080a9237",
+    "url": "/gameotivity2/static/media/Artboard 12.c000a4f3.png"
+  },
+  {
+    "revision": "1f8206f36cebf9c329e2b32eb40922d2",
+    "url": "/gameotivity2/static/media/private sale.1f8206f3.png"
+  },
+  {
+    "revision": "db76be52f811e34a68b87ccc5f35a80f",
+    "url": "/gameotivity2/static/media/news3.db76be52.jpeg"
+  },
+  {
+    "revision": "89fc0d553ac996886e52348964aead49",
+    "url": "/gameotivity2/static/media/news2.89fc0d55.jpeg"
+  },
+  {
+    "revision": "f235a2e2ffe09381cda940f83af48b96",
+    "url": "/gameotivity2/static/media/mockup 10.f235a2e2.png"
+  },
+  {
+    "revision": "aba7dfeffecb1b92b9ba7eeb011fccad",
+    "url": "/gameotivity2/static/media/mockup 2.aba7dfef.png"
+  },
+  {
+    "revision": "8b35b28b6a7ee89511c8720a9feb52c4",
+    "url": "/gameotivity2/static/media/mockup 3.8b35b28b.png"
+  },
+  {
+    "revision": "3de7c0aacdaff85045a63d79fd54bc90",
+    "url": "/gameotivity2/static/media/Artboard 5.3de7c0aa.png"
+  },
+  {
+    "revision": "055915128a59a3445d808edbec7f798a",
+    "url": "/gameotivity2/static/media/Artboard 8.05591512.png"
+  },
+  {
+    "revision": "7b5e9ac642db0e8ab5ace1b64ad0aa12",
+    "url": "/gameotivity2/static/media/Artboard 80.7b5e9ac6.png"
+  },
+  {
+    "revision": "5f7ac065cd9d76334bd36b19886ae51b",
+    "url": "/gameotivity2/static/media/Artboard 7.5f7ac065.png"
+  },
+  {
+    "revision": "ef3464fa73f6c1520254e7444fdcb401",
+    "url": "/gameotivity2/static/media/Artboard 9.ef3464fa.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/gameotivity2/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/gameotivity2/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/gameotivity2/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/gameotivity2/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "50b95f79739f6cc7a626fef370267e46",
+    "url": "/gameotivity2/index.html"
+  }
+];
